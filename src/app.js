80284126f2cbab93239508185ccd8b3466749428
vickey1209 <shrivastav.vickey432@gmail.com>
2024-10-09 09:53:55 +0530

    feat:updated app.js

--- src/app.js
+++ src/app.js
@@ -12,17 +12,17 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-// Clustering Setup
+//cluster setup
 if (cluster.isMaster) {
     console.log(`Master ${process.pid} is running`);
 
-    // Fork workers
+// forks workerd
     for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
     }
 
     cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died`);
+        console.log(`worker ${worker.process.pid} died`);
     });
 } else {
     // Routes
@@ -31,6 +31,6 @@ if (cluster.isMaster) {
 
     // Server Start
     app.listen(PORT, () => {
-        console.log(`Worker ${process.pid} is running on port ${PORT}`);
+        console.log(`Worker ${process.pid} is running on port: ${PORT}`);
     });
 }
