bd814086a7e91906533f670a53e5c91ebdf3235c
vickey1209 <shrivastav.vickey432@gmail.com>
2024-10-09 09:46:05 +0530

    feat:updated db

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 //db connection
 mongoose.connect(process.env.MONGO_URI, {
 
-}).then(() => console.log('MongoDB connected'))
+}).then(() => console.log('database - Mongodb Connected'))
   .catch(err => console.log(err));
 
 module.exports = mongoose;
